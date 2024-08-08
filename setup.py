4fb72841e9980d3d823428e573736770eab95a3c
jamestelzrow <jst94@case.edu>
2024-08-08 16:11:00 -0400

    Specify all subpackages in setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,15 @@ setup(
     version="1.0",
     author="He Ma, Wennie Wang, Siyoung Kim, Man Hin Cheng, Marco Govoni, Giulia Galli",
     author_email="mahe@uchicago.edu",
-    packages=['pycdft'],
+    packages=[
+        "pycdft",
+        "pycdft.atomic",
+        "pycdft.common",
+        "pycdft.constraint",
+        "pycdft.debug",
+        "pycdft.dft_driver",
+        "pycdft.elcoupling"
+    ],
     install_requires=[
         "ase",
         "numpy",
